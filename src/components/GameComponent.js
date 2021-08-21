7fa83ee6abfa5780f0e5a5eea8a6f556e1a5c995
Joseph Edmonds <consultingjde@gmail.com>
2021-08-20 17:11:14 -0700

    remove Typography import

--- src/components/GameComponent.js
+++ src/components/GameComponent.js
@@ -14,7 +14,6 @@ import swal from 'sweetalert';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
 
 import { makeStyles } from '@material-ui/core/styles';
 
